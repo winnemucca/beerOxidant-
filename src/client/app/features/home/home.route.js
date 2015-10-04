dadaf27bc73367163eff66ff47652b5ab039ba4f
Steven Kauyedauty <skauyedauty@gmail.com>
2015-10-03 22:43:40 -0600

    added home page

--- src/client/app/features/home/home.route.js
+++ src/client/app/features/home/home.route.js
@@ -1,17 +1,18 @@
 (function() {
 	'use strict';
-
 	angular
 		.module('beerApp.features.home')
 		.config(config);
 
-		function config($stateProvider) {
+		function config($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise('/home');
+
 			$stateProvider
 				.state('home', {
 					url:'/home',
 					templateUrl: './app/features/home/home.html',
 					controller: 'HomeController',
-					controllerAs: 'vm',
+					controllerAs: 'vm'
 				})
 		} 
 })();
