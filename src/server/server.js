ae304b8a419c83e3dcb1c60985c89a7304148062
Steven Kauyedauty <skauyedauty@gmail.com>
2015-10-14 21:35:02 -0600

    set up

--- src/server/server.js
+++ src/server/server.js
@@ -20,35 +20,43 @@ app.use(logger('dev'));
 
 // app.use('/api', require('./routes'));
 
-console.log('About to crank up node');
 console.log('PORT=' + port);
 console.log('NODE_ENV=' + environment);
+console.log('** DEV **');
+app.use(express.static('./src/client/'));
+app.use(express.static('./'));
+// app.use(express.static('./tmp'));
+// Any invalid calls for templateUrls are under app/* and should return 404
+app.use('/app/*', function(req, res, next) {
+    four0four.send404(req, res);
+});
+// Any deep link calls should return index.html
+app.use('/*', express.static('./src/client/index.html'));
 
-
-switch (environment){
-    case 'build':
-        console.log('** BUILD **');
-        app.use(express.static('./build/'));
-        // Any invalid calls for templateUrls are under app/* and should return 404
-        app.use('/app/*', function(req, res, next) {
-            four0four.send404(req, res);
-        });
-        // Any deep link calls should return index.html
-        app.use('/*', express.static('./build/index.html'));
-        break;
-    default:
-        console.log('** DEV **');
-        app.use(express.static('./src/client/'));
-        app.use(express.static('./'));
-        app.use(express.static('./tmp'));
-        // Any invalid calls for templateUrls are under app/* and should return 404
-        app.use('/app/*', function(req, res, next) {
-            four0four.send404(req, res);
-        });
-        // Any deep link calls should return index.html
-        app.use('/*', express.static('./src/client/index.html'));
-        break;
-}
+// switch (environment){
+//     case 'build':
+//         console.log('** BUILD **');
+//         app.use(express.static('./build/'));
+//         // Any invalid calls for templateUrls are under app/* and should return 404
+//         app.use('/app/*', function(req, res, next) {
+//             four0four.send404(req, res);
+//         });
+//         // Any deep link calls should return index.html
+//         app.use('/*', express.static('./build/index.html'));
+//         break;
+//     default:
+//         console.log('** DEV **');
+//         app.use(express.static('./src/client/'));
+//         app.use(express.static('./'));
+//         // app.use(express.static('./tmp'));
+//         // Any invalid calls for templateUrls are under app/* and should return 404
+//         app.use('/app/*', function(req, res, next) {
+//             four0four.send404(req, res);
+//         });
+//         // Any deep link calls should return index.html
+//         app.use('/*', express.static('./src/client/index.html'));
+//         break;
+// }
 
 app.listen(port, function() {
     console.log('Express server listening on port ' + port);
