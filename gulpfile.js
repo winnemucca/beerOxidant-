c3279b5d356dd4812ef56bd0dbb98e83132805d0
Steven Kauyedauty <skauyedauty@gmail.com>
2015-10-22 05:32:50 -0600

    more gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 // grab gulp packages
-var gulp = require('gulp'),
-	gutil = require('gulp-util');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
-
-gulp.task('default', function() {
-	return gutil.log('Gulp is running');
-})
+gulp.task('vet', function() {
+	return gulp
+		.src([
+			'./src/**/*.js',
+			'./*.js'
+		])
+		.pipe(jscs())
+		.pipe(jshint());
+});
