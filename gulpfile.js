b0531c4376baf5197bda132f84dc31e2537c3486
Steven Kauyedauty <skauyedauty@gmail.com>
2015-10-25 21:23:01 -0600

    new files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
-// grab gulp packages
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
+var args = require('yargs').argv;
+var config = require('./gulp.config')();
+
+var $ = require('gulp-load-plugins')({lazy: true});
+
 
 gulp.task('vet', function() {
+	log('Analyzing source with JSHint and JSCS');
 	return gulp
-		.src([
-			'./src/**/*.js',
-			'./*.js'
-		])
-		.pipe(jscs())
-		.pipe(jshint());
+		.src(config.alljs)
+		.pipe($.if(args.verbose,$.print()))
+		.pipe($.jscs())
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('jshint-stylish',{verbose: true}))
+		.pipe($.jshint.reporter('fail'));
 });
+
+////////////
+///
+function log(msg) {
+	if (typeof(msg) === 'object') {
+		for(var item in msg) {
+			if (msg.hasOwnProperty(item)) {
+				$.util.log($.util.colors.blue(msg[item]));
+			}
+		} 
+	} else {
+			$.util.log($.util.colors.blue(msg));
+	}
+}
